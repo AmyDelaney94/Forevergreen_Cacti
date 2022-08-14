2a9a53f1956879416da9292d15398964753c793b
AmyDelaney94 <amy.delaney.94@gmail.com>
2022-08-14 21:29:23 +0000

    updated shopping cart view

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,10 +1,27 @@
+'''Adding imports to start of file'''
+# from decimal import Decimal
+from django.shortcuts import get_object_or_404
+from products.models import Product
+
+
 def cart_contents(request):
     ''' Calculating total cost in shopping cart '''
 
     cart_items = []
     total = 0
     product_count = 0
-    delivery = 7.99
+    delivery = 7
+    cart = request.session.get('cart', {})
+
+    for item_id, quantity in cart.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        cart_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
     grand_total = delivery + total
 
