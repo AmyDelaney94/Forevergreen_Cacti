f0281b042bbdec935da96b4f4ed6612628356803
AmyDelaney94 <amy.delaney.94@gmail.com>
2022-08-14 19:47:08 +0000

    Add sorting and amount of products to product page

--- products/views.py
+++ products/views.py
@@ -13,14 +13,36 @@ def all_products(request):
     products = Product.objects.all()
     query = None
     categories = None
+    sort = None
+    direction = None
 
     if request.GET:
+        if 'sort' in request.GET:
+            # Allows user to search by name in upper or lower case
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+            
+            if sortkey == 'category':
+                sortkey = 'category__name'
+
+            if 'direction' in request.GET:
+                # If results are decending flip to acesnding
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
         if 'category' in request.GET:
+            # Allows filter of products by individual categories
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
 
         if 'q' in request.GET:
+            # Activates the search bar and shows user error message
             query = request.GET['q']
             if not query:
                 messages.error(request, "You didn't enter a valid search!")
@@ -29,10 +51,13 @@ def all_products(request):
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'products': products,
         'search_term': query,
         'current_categories': categories,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/products.html', context)
