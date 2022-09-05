ee48aa3e7a7c5acbfb77d3ef3bcbc5c70e088f2b
AmyDelaney94 <amy.delaney.94@gmail.com>
2022-09-05 22:03:29 +0000

    Add about us section to site

--- store/views.py
+++ store/views.py
@@ -8,3 +8,9 @@ def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
+
+def about(request):
+    """ A view to return the about page """
+
+    return render(request, 'about.html')
