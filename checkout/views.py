15f34094738f18befc3318d87817770c30c8a91e
AmyDelaney94 <amy.delaney.94@gmail.com>
2022-09-16 15:18:56 +0000

    add stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -15,6 +15,7 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51LifPjG8ZOXHqvwWNC42MjHyaOBg05Q8qDhCCmwz5awtPgdHtZUjKzhk9nbJ776ggqQXWj5HrQ2n4DX6N0IF1xA9009cTykESV',
     }
 
     return render(request, template, context)
