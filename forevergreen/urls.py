1bccacd291ffb0ebf336a193a05e91b88f3133a9
AmyDelaney94 <amy.delaney.94@gmail.com>
2022-09-22 22:47:14 +0000

    Add link to admin panel to nav bar

--- forevergreen/urls.py
+++ forevergreen/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls'), name='admin'),
     path('accounts/', include('allauth.urls')),
     path('', include('store.urls')),
     path('products/', include('products.urls')),
