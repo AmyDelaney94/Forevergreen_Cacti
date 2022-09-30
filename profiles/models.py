6faa4811e89af25607c3416b62f9b80308ac9b37
AmyDelaney94 <amy.delaney.94@gmail.com>
2022-09-30 18:48:51 +0000

    Add country field to checkout and user profile

--- profiles/models.py
+++ profiles/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django_countries.fields import CountryField
 
 
 class UserProfile(models.Model):
@@ -20,6 +21,8 @@ class UserProfile(models.Model):
                                             null=True, blank=True)
     default_county = models.CharField(max_length=80, null=True, blank=True)
     default_eircode = models.CharField(max_length=20, null=True, blank=True)
+    default_country = CountryField(blank_label='Country',
+                                   null=True, blank=True)
 
     def __str__(self):
         return self.user.username
