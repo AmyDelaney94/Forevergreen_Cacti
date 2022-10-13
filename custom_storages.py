7decb59f46a9000517a6049c457be02c5120fa23
AmyDelaney94 <amy.delaney.94@gmail.com>
2022-10-13 17:12:35 +0000

    Add AWS s3 for storage of static files to Heroku

---
+++ custom_storages.py
@@ -0,0 +1,13 @@
+'''Adding imports to beginning of file'''
+from django.conf import settings
+from storages.backends.s3boto3 import S3Boto3Storage
+
+
+class StaticStorage(S3Boto3Storage):
+    '''Adding class for static file storage using AWS'''
+    location = settings.STATICFILES_LOCATION
+
+
+class MediaStorage(S3Boto3Storage):
+    '''Adding class for media file storage using AWS'''
+    location = settings.MEDIAFILES_LOCATION
