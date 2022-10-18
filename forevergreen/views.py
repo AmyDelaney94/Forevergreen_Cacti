0a75363213f2c1f259e0d9a59cc663c34d6430c7
AmyDelaney94 <amy.delaney.94@gmail.com>
2022-10-18 11:23:24 +0000

    testing error404 page view  in heroku

--- forevergreen/views.py
+++ forevergreen/views.py
@@ -2,9 +2,9 @@
 from django.shortcuts import render
 
 
-# def handler404(request, exception):
-#     """ Error Handler 404 - Page Not Found """
-#     return render(request, "errors/404.html", status=404)
+def handler404(request, exception):
+    """ Error Handler 404 - Page Not Found """
+    return render(request, "errors/404.html", status=404)
 
 
 # def handler400(request, exception):
